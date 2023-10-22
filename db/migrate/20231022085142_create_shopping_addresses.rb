f4465e95c4cd1944e36abd292ba088fd31ec88d8
”Ryopu510sm” <pukun510.mssp22@gmail.com>
2023-10-22 09:45:58 +0000

    Create-table-items

---
+++ db/migrate/20231022085142_create_shopping_addresses.rb
@@ -0,0 +1,11 @@
+class CreateShoppingAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shopping_addresses do |t|
+      t.integer :customer_id, null: false #会員ID
+      t.string :postal_code, null: false #郵便番号
+      t.string :address, null: false #住所
+      t.string :name, null: false #宛名
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20231022085849_create_ordered_products.rb
@@ -0,0 +1,12 @@
+class CreateOrderedProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ordered_products do |t|
+      t.integer :item_id, null: false #商品ID
+      t.integer :order_id, null:false #注文ID
+      t.integer :tax_included, null:false #税込価格
+      t.integer :amount, null: false #個数
+      t.integer :production_status, null:false #製作ステータス
+      t.timestamps
+    end
+  end
+end
